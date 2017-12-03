75ef65fde607b419d90335ea1539ef5415a40f86
Jarvis <novosad_msu@mail.ru>
2017-12-03 16:15:25 +0300

    ??

--- container_class.hpp
+++ container_class.hpp
@@ -1,34 +1,42 @@
 #ifndef _CONTAINER_CLASS_H_
 #define _CONTAINER_CLASS_H_
 
+#include <iostream>
 #include <vector>
-#include <utility> // pair
+//#include <utility> // pair
 #include <algorithm> //sort
-#include <map>
+#include <set>
 
 
 
 class Container {
 public:
     Container(std::vector<int> items, std::vector<int> cont_sizes): _items(items), _cont_sizes(cont_sizes) {}
-    std::map<int, int> sort_items();
+    double sort_items(double jando);
+    void perebor(int item, double jadno, std::vector<int> item_container, std::vector<int> item_cont, int &best_size);
 private:
     std::vector<int> _items;
     std::vector<int> _cont_sizes;
 };
 
 
-std::map<int, int> Container::sort_items() {
+double Container::sort_items(double jadno) {
 
-    std::map<int, int> item_container; // our map (item : container)
+    std::vector<int> item_container(_items.size());
 
     std::sort(_items.begin(), _items.end()); // sort item_sizes
+
+    std::vector<int> cont_sizes(_cont_sizes);
+
+    int size = _cont_sizes.size();
+
+    perebor(0, jadno, item_container, cont_sizes, size);
 
     for (int item = 0; item < _items.size(); item++) {
         bool in_container = false;
         for (int container = 0; container < _cont_sizes.size(); container++)
             if (_items[item] <= _cont_sizes[container]) {
-                item_container.insert(std::make_pair<int, int>(item + 1, container + 1));
+                item_container[item] = container;
                 _cont_sizes[container] -= _items[item];
                 in_container = true;
                 break;
@@ -40,7 +48,61 @@ std::map<int, int> Container::sort_items() {
             );
     }
 
-    return item_container;
+    std::set<int> my_set;
+    for (int i = 0; i < _items.size(); i++)
+        my_set.insert(item_container[i]);
+
+    return size / my_set.size();
+}
+
+
+void Container::perebor(int item, double jadno, std::vector<int> item_container, std::vector<int> cont_sizes, int &best_size) {
+
+    //printf("Perebor: %d\n", item);
+    if (item == int(_items.size() * jadno)) {
+
+        for (int item_ = item; item_< _items.size(); item_++) {
+            bool in_container = false;
+            for (int container = 0; container < _cont_sizes.size(); container++)
+                if (_items[item_] <= cont_sizes[container]) {
+                    item_container[item_] = container;
+                    cont_sizes[container] -= _items[item_];
+                    in_container = true;
+                    break;
+                }
+            if (!in_container)
+                printf(
+                        "\nImpossible to put item %d(size %d) in any container, change container sizes\n",
+                        item + 1, _items[item_]
+                );
+        }
+
+        std::set<int> my_set;
+        for (int i = 0; i < _items.size(); i++)
+            my_set.insert(item_container[i]);
+
+        if (my_set.size() < best_size)
+            best_size = my_set.size();
+
+        return;
+    }
+
+    bool in_container = false;
+    for (int container = 0; container < _cont_sizes.size(); container++) {
+        if (_items[item] <= cont_sizes[container]) {
+            item_container[item] = container;
+            cont_sizes[container] -= _items[item];
+            in_container = true;
+            perebor(item + 1, jadno, item_container, cont_sizes, best_size);
+            if (item != 0)
+                cont_sizes[item_container[item]] += _items[item];
+        }
+    }
+    if (!in_container)
+        printf(
+                "\nImpossible to put item %d(size %d) in any container, change container sizes\n",
+                item + 1, _items[item]
+        );
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <stdlib.h>
+#include <time.h>
 #include "container_class.hpp"
 
-#define N 10
-#define S 5
+#define N 100
+#define S 15
 
 
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
+    if (argc < 2) {
         printf("\nPlease, write filename\n");
         return -1;
     }
+
+    double jadno = 1.0;
+    if (argc == 3)
+        jadno = atof(argv[2]);
+
     std::ifstream file(argv[1]);
 
     std::vector<int> items(N);
     std::vector<int> cont_sizes (S);
 
-    std::string s;
+ /*   std::string s;
     std::stringstream ss1;
     std::stringstream ss2;
 
@@ -31,13 +38,24 @@ int main(int argc, char **argv) {
     ss2 << s;
     for (int i = 0; i < S; i++)
         ss2 >> cont_sizes[i];
+        */
+    for (int i = 0; i < N; i++)
+        items[i] = rand() % 10 + 1;
+    for (int i = 0; i < S; i++)
+        cont_sizes[i] = rand() % 200 + 20;
+
+    printf("Jadniy algorithm: %lf\n", jadno);
+
+    time_t start = clock();
 
     Container container(items, cont_sizes);
-    std::map<int, int> result(container.sort_items());
+    double result = container.sort_items(jadno);
 
-    for (std::map<int, int>::iterator it = result.begin(); it != result.end(); it++)
-        printf("\nitem %d is on %d container\n", it->first, it->second);
+    time_t end = clock();
 
+    printf("Result: %lf\n", result);
+
+    printf("\nExecuting time: %lf\n", (double)(end - start) / CLOCKS_PER_SEC);
     file.close();
     return 0;
 }
